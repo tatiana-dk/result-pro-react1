950a2345cabeb52ceea14984d3b28f9ad7abc75e
Tatiana <kartasheva.tatiana@gmail.com>
2026-03-06 15:47:34 +0300

    feat: add basic to useFetch

--- src/useFetch.js
+++ src/useFetch.js
@@ -1,1 +1,19 @@
-export function useFetch() {}
+import { useRef } from "react";
+
+export function useFetch() {
+
+    const data = useRef();
+
+    const isLoading = useRef(false);
+
+    const error = useRef('');
+
+    function refetch() {}
+
+    return {
+        data,
+        isLoading,
+        error,
+        refetch
+    }
+}
