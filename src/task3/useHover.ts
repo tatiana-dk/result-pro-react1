8c515b7ce1176566d377951789a996be645b4dd7
Tatiana <kartasheva.tatiana@gmail.com>
2026-03-08 18:34:29 +0300

    feat: add useHover logic

--- src/task3/useHover.ts
+++ src/task3/useHover.ts
@@ -1,1 +1,36 @@
-export function useHover() {}
+import { useRef, useState, useEffect, useCallback } from "react"
+
+export function useHover() {
+
+    const ref = useRef<HTMLDivElement>(null);
+    const [hovered, setHovered] = useState<boolean>(false);
+
+    const handleMouseenter = useCallback(() => {
+        setHovered(true);
+    }, []);
+
+    const handleMouseleave = useCallback(() => {
+        setHovered(false);
+    }, []);
+
+
+    useEffect(() => {
+        if (ref.current) {
+            ref.current.addEventListener('mouseenter', handleMouseenter);
+            ref.current.addEventListener('mouseleave', handleMouseleave);
+        }
+
+        return () => {
+            if (ref.current) {
+                ref.current.removeEventListener('mouseenter', handleMouseenter);
+                ref.current.removeEventListener('mouseleave', handleMouseleave);
+            }
+        };
+    }, [ref.current]);
+    
+
+    return {
+        hovered,
+        ref
+    }
+}
