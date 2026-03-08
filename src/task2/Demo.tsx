be2e6194a52e554523c1630c43bc4eb9417e90e5
Tatiana <kartasheva.tatiana@gmail.com>
2026-03-08 14:04:10 +0300

    feat: add Demo component

---
+++ src/task2/Demo.tsx
@@ -0,0 +1,15 @@
+import { useLocalStorage } from "./useLocalStorage";
+
+export function Demo() {
+    const [value, { setItem, removeItem }] = useLocalStorage('some-key');
+
+  return (
+    <div>
+      <p>Значение из LocalStorage: {value}</p>
+      <div>
+        <button onClick={() => setItem('new storage value')}>Задать значение</button>
+        <button onClick={() => removeItem()}>Удалить значение</button>
+      </div>
+    </div>
+  );
+}
