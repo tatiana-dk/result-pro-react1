98999bf3c32876be1b58ad9b0999a84c040f8dff
Tatiana <kartasheva.tatiana@gmail.com>
2026-03-08 15:06:18 +0300

    feat: add useCallback to useLocalStorage

--- src/task2/useLocalStorage.tsx
+++ src/task2/useLocalStorage.tsx
@@ -1,18 +1,19 @@
+import { useRef, useState, useCallback } from 'react';
 import type {LocalStorageSetValue, LocalStorageReturnValue, UseLocalStorage} from './types';
 
 export const useLocalStorage: UseLocalStorage = (key) => {
+    const localStorageRef = useRef(window.localStorage);
+    const [value, setValue] = useState<LocalStorageReturnValue>(localStorageRef.current.getItem(key));
 
-    const localStorage = window.localStorage;
-    
-    const value: LocalStorageReturnValue = localStorage.getItem(key);
+    const setItem = useCallback((newValue: LocalStorageSetValue) => {
+        localStorageRef.current.setItem(key, newValue);
+        setValue(newValue);
+    }, [key]);
 
-    const setItem = (newValue: LocalStorageSetValue) => {
-        localStorage.setItem(key, newValue);
-    };
-
-    const removeItem = () => {
-        localStorage.removeItem(key);
-    }
+    const removeItem = useCallback(() => {
+        localStorageRef.current.removeItem(key);
+        setValue('');
+    }, []);
 
     return [
         value,
